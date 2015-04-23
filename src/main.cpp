600c88642c0c4ddc7729ba81a9055333dcc42bbf
Adel Ahmadyan <ahmadyan@gmail.com>
2015-04-23 16:07:07 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  duplex
+//
+//  Created by Adel Ahmadyan on 4/23/15.
+//  Copyright (c) 2015 Adel Ahmadyan. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "Duplex" << endl ;
+    
+}
