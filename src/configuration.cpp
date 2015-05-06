af2c3b62b3a7844ceb3810c5db139d37636e6973
ahmadyan <ahmadyan@gmail.com>
2015-05-05 22:42:09 -0500

    structuring

--- src/configuration.cpp
+++ src/configuration.cpp
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 //
 //  Configuration.cpp
 //  duplex
@@ -6,13 +5,10 @@
 //  Created by Adel Ahmadyan on 5/5/15.
 //  Copyright (c) 2015 Adel Ahmadyan. All rights reserved.
 //
-=======
->>>>>>> Stashed changes
 
 #include "configuration.h"
 
 Configuration::Configuration(string filename){
-<<<<<<< Updated upstream
 	cout << "Loading configuration" << endl;
 	property_tree::read_xml(filename, pointerTree);
 
@@ -37,24 +33,3 @@ Configuration::Configuration(string filename){
 Configuration::~Configuration(){
 
 }
-=======
-    property_tree::read_xml( filename, pointerTree  );
-    
-    // Traverse property tree example
-    BOOST_FOREACH( property_tree::ptree::value_type const& node, pointerTree.get_child( "system.items" ) ){
-        property_tree::ptree subtree = node.second;
-        
-        if( node.first == "item" ){
-            BOOST_FOREACH( property_tree::ptree::value_type const& v, subtree.get_child( "" ) ){
-                string label = v.first;
-                if ( label != "<xmlattr>" ){
-                    string value = subtree.get<string>( label );
-                    cout << label << ":  " << value << endl;
-                }
-            }
-            cout << endl;
-        }
-    }
-
-}
->>>>>>> Stashed changes
