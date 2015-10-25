282386c1121dc2a4650dcf0c53fb0a447f04eb13
Adel Ahmadyan <ahmadyan@gmail.com>
2015-10-24 21:59:48 -0500

    Added HSPICE class

---
+++ src/hspice.cpp
@@ -0,0 +1,19 @@
+#include "hspice.h"
+#include <cmath>
+
+Hspice::Hspice(Settings*){}
+Hspice::~Hspice(){}
+
+
+
+void Hspice::generateNetlist(){
+
+}
+
+void Hspice::runSimulation(){
+
+}
+
+void Hspice::parseSimulationLog(){
+
+}
---
+++ src/hspice.h
@@ -0,0 +1,20 @@
+//
+//  hspice.h
+//  duplex
+//
+//  Created by Adel Ahmadyan on 10/23/15.
+//  Copyright (c) 2015 Adel Ahmadyan. All rights reserved.
+//
+#pragma once
+#include "configuration.h"
+#include "state.h"
+
+class Hspice{
+public:
+	Hspice(Settings* config);
+	~Hspice();
+
+	void generateNetlist();
+	void runSimulation();
+	void parseSimulationLog();
+};
