b671c5c5baf911b51ee093fe47e22d0437099e0b
Adel Ahmadyan <ahmadyan@gmail.com>
2015-11-22 20:58:29 -0600

    exporting duplex save file to matlab for rendering

--- test/duplex2matlab.py
+++ test/duplex2matlab.py
@@ -14,8 +14,8 @@ def main():
         workingDirectory = "C:\\Users\\adel\\code\\Duplex\\bin"
         duplexCommand = "C:\\Users\\adel\\code\\Duplex\\DuplexVS\\DuplexVS.exe"
         configfile = "C:\\Users\\adel\\code\\Duplex\\bin\\inverter.cfg"
-        matlabfile = "C:\\Users\\adel\\code\\Duplex\\bin\\yield_samples.m"
-        duplexSavefile = "C:\\Users\\adel\\code\\Duplex\\bin\\opamp_result1.xml"
+        matlabfile = "C:\\Users\\adel\\code\\Duplex\\bin\\duplex_figure.m"
+        duplexSavefile = "C:\\Users\\adel\\code\\Duplex\\bin\\opamp_result2.xml"
     else:
         workingDirectory = "/Users/adel/code/Duplex/bin/"
         duplexCommand = '/Users/adel/code/Duplex/xcode/Duplex.xcode/build/Debug/Duplex.xcode'
@@ -24,24 +24,51 @@ def main():
 
     os.chdir(workingDirectory)
     root = xml.etree.ElementTree.parse(duplexSavefile).getroot()
-
+    data = []
+    pdata = []
     for p in root.findall('./data/node'):
-        for obj in p.iter('objective'):
-            objectives = obj.text.split()
-            for i,x in enumerate(objectives):
-                objectives[i]=float(x)
-        for param in p.iter('parameter'):
-            parameters = param.text.split()
-            for i,x in enumerate(parameters):
-                parameters[i]=float(x)
+        for parent in p.iter('parent'):
+            id=-1
+            for me in p.iter('id'):
+                id = int( me.text )
+            for obj in p.iter('objective'):
+                objectives = obj.text.split()
+                for i,x in enumerate(objectives):
+                    objectives[i]=float(x)
+            for param in p.iter('parameter'):
+                parameters = param.text.split()
+                for i,x in enumerate(parameters):
+                    parameters[i]=float(x)
+            parent_id = int(parent.text)
+            if (id!=0):
+                pdata.append( data[parent_id] )
+            else:
+                pdata.append( str(parameters[0]) + ' ' + str(parameters[1]) )
+            data.append(str(parameters[0]) + ' ' + str(parameters[1]))
 
+    print(data)
+    print (pdata)
 
-    size = len(pareto)
-    variableSize = 2
+    size = len(data)
+
     with open(matlabfile, 'w') as f:
-        f.write('yield=zeros('+ str(size) +','+ str(variableSize) +');\n')
+        f.write('duplex=zeros('+ str(size) +','+ str(4) +');\n')
         for i in range(1, size+1):
-            f.write('yield('+str(i)+',:)=' + str(pareto[i-1]) +';\n')
-        f.write('scatter(yield(:,1), yield(:,2))\n')
+            f.write('duplex('+str(i)+',:)=[ ' + str(data[i-1]) + ' ' + str(pdata[i-1]) + ' ];\n')
 
 if  __name__ =='__main__':main()
+
+# Code for drawing the random tree in MATLAB
+#x = unique(m(:, 1));
+#y = unique(m(:,2));
+#contour(y, x, bw);
+#hold on
+#for i=1:500,
+#    x1 = duplex(i, 1);
+#    y1 = duplex(i, 2);
+#    x2 = duplex(i, 3);
+#    y2 = duplex(i, 4);
+
+#    line( [x1 x2], [y1 y2], 'LineWidth', 1, 'Color', 'k' );
+#end
+#hold off
