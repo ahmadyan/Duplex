0aa0014ddfd9cacf03fa07c04a757593f5bbeb8e
Adel Ahmadyan <ahmadyan@gmail.com>
2016-06-12 11:19:29 -0500

    Implemented Nestrov momentum in gradient descent algorithm

--- src/gradientDescent.cpp
+++ src/gradientDescent.cpp
@@ -1,15 +1,19 @@
 #include "gradientDescent.h"
 
 GradientDescent::GradientDescent(Settings* s):Optimizer(s){
-    learning_rate=settings->lookupFloat("optimization.learning_rate");
+    learning_rate_base=settings->lookupFloat("optimization.learning_rate");
     decay = settings->lookupFloat("optimization.decay");
     momentum = settings->lookupFloat("optimization.momentum");
+    nestrov = settings->check("optimization.nestrov", "true");
     velocity=0;
-    cout << "Initializing Gradient Descent optimizer" << endl ;
+    cout << "Initializing Gradient Descent optimizer complete"
+         << " , Nestrov="  << nestrov
+         << " , Momentum=" << momentum
+         << " , Decay rate=" << decay
+         << " , learning rate=" << learning_rate_base << endl;
 }
 
-GradientDescent::~GradientDescent(){
-}
+GradientDescent::~GradientDescent(){}
 
 bool GradientDescent::hasGradientInformation(){
     return true;
@@ -18,18 +22,29 @@ bool GradientDescent::hasGradientInformation(){
 State* GradientDescent::update(State* u){
     auto pSize = u->getParameterSize();
     auto objSize = u->getObjectiveSize();
-    double* prev = u->getParameter();
+    auto prev = u->getParameter();
     auto iterations = u->getID();
+    
+    auto v = new State(pSize, objSize);
+    auto input = new double[pSize]();
+    
     // decayed learning rate:
-    auto lr = learning_rate * (1.0 / (1.0 + decay * iterations));
-
-    State* v = new State(pSize, objSize);
-    double* input = new double[pSize]();
-    //buggy, how to model multi-objective functions here?
+    auto learning_rate = learning_rate_base * (1.0 / (1.0 + decay * iterations));
+    
+    // we store the previous value of velocity to be used in Nestrov's momentum calculation
+    previous_velocity=velocity;
+    
     for(int i=0;i<u->getParameterSize();i++){
-        velocity = momentum*velocity - lr*u->getDerivative(0, i);       //momentum rule
-        input[i]= prev[i] + velocity;                                   //descent rule
-
+        auto dx = u->getDerivative(0, i);
+        // Momentum update rule
+        velocity = momentum*velocity - learning_rate*dx;
+        if(nestrov){
+            // Updating the state according to Nestrov momentum
+            input[i] = prev[i] - momentum*previous_velocity + (1+momentum)*velocity;
+        }else{
+            // Descent rule for the Vanilla Gradient Descent (with momentum, but without Nestrov).
+            input[i] = prev[i] + velocity;
+        }
         if(input[i]<-1) input[i]=-1;
         if(input[i]>1) input[i]=1;
     }
--- src/gradientDescent.h
+++ src/gradientDescent.h
@@ -3,11 +3,17 @@
 #include <iostream>
 using namespace std;
 
+// The gradient descent optimization algorithm
+// With support for momentum, learning rate decay and Nestrov accelerated momentum
 class GradientDescent : public Optimizer{
-    double learning_rate;
-    double decay;
-    double velocity;
-    double momentum;
+    double learning_rate_base;
+    double decay;               // the decay value of the learning rate
+    double velocity;            // the velocity paramter for momentum update
+    double momentum;            // dampens the velocity during momentum update
+    double previous_velocity;   // previous value of velocity, used in Nestrov momentum equations.
+    
+    bool nestrov;               // flag that enables the Nestrov equations
+    
 public:
     GradientDescent(Settings* s);
     ~GradientDescent();
