7e4c9aa68f2ec0c88282f61f6d6e672d4622ec65
Adel Ahmadyan <ahmadyan@gmail.com>
2017-04-25 22:16:11 -0700

    Add config4cpp as a submodule

--- cmake/Findconfig4cpp.cmake
+++ cmake/Findconfig4cpp.cmake
@@ -1,7 +1,7 @@
 find_library(CONFIG4CPP_LIBRARY config4cpp
-  PATH ${PROJECT_SOURCE_DIR}/libraries/config4cpp/lib
+  PATH ${PROJECT_SOURCE_DIR}/submodules/config4cpp/lib
 )
 
 find_path(CONFIG4CPP_INCLUDE_DIR "config4cpp/Configuration.h"
-  PATH ${PROJECT_SOURCE_DIR}/libraries/config4cpp/include
+  PATH ${PROJECT_SOURCE_DIR}/submodules/config4cpp/include
 )
